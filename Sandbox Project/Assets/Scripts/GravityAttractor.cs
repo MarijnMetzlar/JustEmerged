a0f48bace2917ee78acb36237c79279ea66840ae
MarijnMetzlar <marijn_jacob@hotmail.com>
2015-11-18 21:50:02 +0100

    Added gravity of the world
    Works already fine, but some objects look like they are above the
    ground!

---
+++ Sandbox Project/Assets/Scripts/GravityAttractor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityAttractor : MonoBehaviour {
+
+	//added to the world!
+
+	public float gravity = -10.0f;
+	
+	public void Attract(Transform body)
+	{
+		Vector3 targetDir = (body.position - transform.position).normalized;
+		Vector3 bodyUp = body.up;
+
+		body.rotation = Quaternion.FromToRotation (bodyUp, targetDir) * body.rotation;
+		body.GetComponent<Rigidbody>().AddForce (targetDir * gravity);
+	}
+}
---
+++ Sandbox Project/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+
+public class GravityBody : MonoBehaviour {
+
+	//added to every object that is on the world
+
+	GravityAttractor planet;
+
+	void Awake()
+	{
+		planet = GameObject.FindGameObjectWithTag ("Planet").GetComponent<GravityAttractor> ();
+		GetComponent<Rigidbody> ().useGravity = false;
+		GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeRotation;
+	}
+
+	void FixedUpdate()
+	{
+		planet.Attract (transform);
+	}
+}
