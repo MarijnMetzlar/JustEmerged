a6398376c780aca6105ad1e71307303ef44daa95
MarijnMetzlar <marijn_jacob@hotmail.com>
2015-12-11 15:13:53 +0100

    Teleportation
    Maybe we will implement this different, for now it is done

---
+++ Sandbox Project/Assets/Scripts/PlayerTeleportation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTeleportation : MonoBehaviour {
+
+	void Update () 
+	{
+		Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position;
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+
+		if (playerPosition.z > 2950.0f) 
+		{
+			player.GetComponent<Transform>().position = new Vector3(playerPosition.x, playerPosition.y, 85.0f);
+		}
+
+		else if (playerPosition.z < 50.0f) 
+		{
+			player.GetComponent<Transform>().position = new Vector3(playerPosition.x, playerPosition.y, 2915.0f);
+		}
+
+		if (playerPosition.x > 2950.0f) 
+		{
+			player.GetComponent<Transform>().position = new Vector3(85.0f, playerPosition.y, playerPosition.z);
+		}
+
+		if (playerPosition.x < 50.0f) 
+		{
+			player.GetComponent<Transform> ().position = new Vector3 (2915.0f, playerPosition.y, playerPosition.z);
+		}
+	}
+}
