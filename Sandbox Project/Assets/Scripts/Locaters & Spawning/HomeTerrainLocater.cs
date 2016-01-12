01e8f64a7823887f575a9082e7d22000d76caf0d
MarijnMetzlar <marijn_jacob@hotmail.com>
2016-01-12 12:03:01 +0100

    Put the correct cars in all the boss scenes, changed the world scene a bit
    Spawning car in front of the garage. The garage spawns on one place for
    now!

--- Sandbox Project/Assets/Scripts/Locaters & Spawning/HomeTerrainLocater.cs
+++ Sandbox Project/Assets/Scripts/Locaters & Spawning/HomeTerrainLocater.cs
@@ -7,7 +7,6 @@ public class HomeTerrainLocater : MonoBehaviour {
 	//public Transform player;
 
 	public int garageLocation;
-	public Vector3 garagePosition;
 
 	// Use this for initialization
 	void Start () 
@@ -15,30 +14,27 @@ public class HomeTerrainLocater : MonoBehaviour {
 		//find specific location on the home realm!
 		Vector3 homePosition = (GameObject.FindGameObjectWithTag ("LocationHome").GetComponent<Transform>().position);
 		//3 different locations for the garage
-		garageLocation = Random.Range (0, 5);
+		garageLocation = 1; //Random.Range (0, 5);
 
 		if (garageLocation == 0) 
 		{
-			garagePosition = new Vector3 (homePosition.x + 40.0f, homePosition.y + 20.0f, homePosition.z);
+			Instantiate (garagePrefab, new Vector3 (homePosition.x + 40.0f, 50.0f, homePosition.z), Quaternion.identity);
 		} 
 		else if (garageLocation == 1) 
 		{
-			garagePosition = new Vector3 (homePosition.x + 250.0f, homePosition.y - 30.5f, homePosition.z);
+			Instantiate (garagePrefab, new Vector3 (homePosition.x + 250.0f, 50.0f, homePosition.z), Quaternion.identity);
 		} 
 		else if(garageLocation == 2) 
 		{
-			garagePosition = new Vector3 (homePosition.x, homePosition.y - 30.5f, homePosition.z + 250.0f);
+			Instantiate (garagePrefab, new Vector3 (homePosition.x, 50.0f, homePosition.z + 250.0f), Quaternion.identity);
 		}
 		else if(garageLocation == 3) 
 		{
-			garagePosition = new Vector3 (homePosition.x - 250.0f, homePosition.y - 30.5f, homePosition.z);
+			Instantiate (garagePrefab, new Vector3 (homePosition.x - 250.0f, 50.0f, homePosition.z), Quaternion.Euler(0, 90, 0));
 		}
 		else if(garageLocation == 4) 
 		{
-			garagePosition = new Vector3 (homePosition.x, homePosition.y - 30.5f, homePosition.z - 250.0f);
+			Instantiate (garagePrefab, new Vector3 (homePosition.x, 50.0f, homePosition.z - 250.0f), Quaternion.identity);
 		}
-
-		//Instantiate (player, homePosition, Quaternion.identity);
-		Instantiate (garagePrefab, garagePosition, Quaternion.identity);
 	}
 }
