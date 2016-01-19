05f245d824db60ba241374d471d0f631b9a69e1a
MarijnMetzlar <marijn_jacob@hotmail.com>
2016-01-20 00:48:01 +0100

    Text for tutorial shows up, every 5 sec.
    Too lazy to fill everything in. I will let Tagor tweak around with it
    and make it better! If the timer is not how we want it, I can still
    change it

---
+++ Sandbox Project/Assets/Scripts/TutorialText/TutorialText.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TutorialText : MonoBehaviour {
+
+	private Text txt;
+	private float changeTextTimer = 5.0f;
+	private int textOrder = 0;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		txt = gameObject.GetComponent<Text> ();
+		txt.text = "Press the spacebar!";
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		changeTextTimer -= Time.deltaTime;
+		if (changeTextTimer < 0) 
+		{
+			if(textOrder == 0)
+			{
+				txt.text = "You can drive by using ASDW";
+				textOrder += 1;
+			}
+
+			else if(textOrder == 1)
+			{
+				txt.text = "Fill in the rest tagor";
+				textOrder += 1;
+			}
+
+			else if(textOrder == 2)
+			{
+				txt.text = "Fill in the rest tagor2";
+				textOrder += 1;
+			}
+
+			else if(textOrder == 3)
+			{
+				txt.text = "Fill in the rest tagor3";
+				textOrder += 1;
+			}
+
+			else if(textOrder == 4)
+			{
+				txt.text = "Fill in the rest tagor4";
+				textOrder += 1;
+			}
+
+			else if(textOrder == 5)
+			{
+				txt.text = "Fill in the rest tagor5";
+				textOrder += 1;
+			}
+
+			changeTextTimer = 5.0f;
+		}
+	}
+}
