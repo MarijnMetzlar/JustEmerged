821988fbbfdaa029b86a28af9454c85a2e667ca7
MarijnMetzlar <marijn_jacob@hotmail.com>
2016-01-25 12:59:41 +0100

    Tutorial Sound (and text)

--- Sandbox Project/Assets/Scripts/TutorialText/TutorialText.cs
+++ Sandbox Project/Assets/Scripts/TutorialText/TutorialText.cs
@@ -5,109 +5,117 @@ using UnityEngine.UI;
 public class TutorialText : MonoBehaviour {
 
 	private Text txt;
-	public float changeTextTimer = 4.0f;
-	public int textOrder = 0;
 	private bool showText;
+
+	public AudioSource welcome;
+	public AudioSource steering;
+	public AudioSource gimble;
+	public AudioSource shooting;
+	public AudioSource jumping;
+	public AudioSource strafing;
+	public AudioSource nitro;
+	public AudioSource flip;
+	public AudioSource hull;
 	
 	// Use this for initialization
 	void Start () 
 	{
 		txt = gameObject.GetComponent<Text> ();
 		txt.text = "Welcome, this is your garage! Switch & upgrade your items here by clicking";
+		welcome.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		changeTextTimer -= Time.deltaTime;
-		if (changeTextTimer < 0) 
+		//sounds display!
+
+		if (welcome.time > 24.1f) 
 		{
-			if(textOrder == 0)
-			{
-				txt.text = "You can see your car details in the garage";
-				textOrder += 1;
-				changeTextTimer = 4.0f;
-			}
-
-			else if(textOrder == 1)
-			{
-				txt.text = "Press the leave garage button to start your adventure!";
-				textOrder += 1;
-				changeTextTimer = 4.0f;
-			}
-
-			else if(textOrder == 2)
-			{
-				txt.text = "You can drive your car by using: W,A,S,D";
-				textOrder += 1;
-				changeTextTimer = 0.0f;
-			}
-
-			else if(textOrder == 3)
-			{
-				if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.W))
-				{
-					txt.text = "Click your right mouse button to enter or exit gimbal mode";
-					textOrder += 1;
-				}
-				changeTextTimer = 0.0f;
-			}
-
-			else if(textOrder == 4)
-			{
-				if(Input.GetMouseButtonDown(1))
-				{
-					txt.text = "In gimbal mode you can shoot with your left mouse button";
-					textOrder += 1;
-				}
-				changeTextTimer = 0.0f;
-			}
-
-			else if(textOrder == 5)
-			{
-				if(Input.GetMouseButtonDown(0))
-				{
-					txt.text = "By pressing B you can do a short jump";
-					textOrder += 1;
-				}
-				changeTextTimer = 0.0f;
-			}
-
-			else if(textOrder == 6)
-			{
-				if(Input.GetKeyDown(KeyCode.B))
-				{
-					txt.text = "You can make a strafe dash by pressing Q,E";
-					textOrder += 1;
-				}
-				changeTextTimer = 0.0f;
-			}
-
-			else if(textOrder == 7)
-			{
-				if(Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E))
-				{
-					txt.text = "Pressing shift will activate your nitro";
-					textOrder += 1;
-				}
-				changeTextTimer = 0.0f;
-			}
-
-			else if(textOrder == 8)
-			{
-				if(Input.GetKeyDown(KeyCode.LeftShift))
-				{
-					txt.text = "If you are flipped, then you can turn around by pressing L";
-					textOrder += 1;
-					changeTextTimer = 4.0f;
-				}
-			}
-			
-			else if(textOrder == 9)
-			{
-				txt.text = null;
-				changeTextTimer = 0.0f;
-			}
+			welcome.Stop ();
+			txt.text = "You can drive your car by using: W,A,S,D";
+			steering.Play ();
 		}
+
+		if (steering.time > 19.95f) 
+		{
+			steering.Stop ();
+			txt.text = "Click your right mouse button to enter or exit gimbal mode";
+			gimble.Play ();
+		}
+
+		if (gimble.time > 21.45f) 
+		{
+			gimble.Stop ();
+			txt.text = "In gimbal mode you can shoot with your left mouse button";
+			shooting.Play ();
+		}
+
+		if (shooting.time > 37.20f) 
+		{
+			shooting.Stop ();
+			txt.text = "By pressing B you can do a short jump";
+			jumping.Play ();
+		}
+
+		if (jumping.time > 17.80f) 
+		{
+			jumping.Stop ();
+			txt.text = "You can make a strafe dash by pressing Q,E";
+			strafing.Play ();
+		}
+
+		if (strafing.time > 16.70f) 
+		{
+			strafing.Stop ();
+			txt.text = "Pressing shift will activate your nitro";
+			nitro.Play ();
+		}
+
+		if (nitro.time > 20.40f) 
+		{
+			nitro.Stop ();
+			txt.text = "If you are flipped, then you can turn around by pressing L";
+			flip.Play ();
+		}
+
+		if (flip.time > 22.20f) 
+		{
+			flip.Stop ();
+			txt.text = "JA OK";
+			hull.Play ();
+		}
+
+		if (hull.time > 21.35f) 
+		{
+			hull.Stop ();
+			txt.text = null;
+			StopText ();
+		}
+
+		if(Input.GetKeyDown(KeyCode.Backspace))
+		{
+			StopText();
+		}
+
+		if(Input.GetKeyDown(KeyCode.RightShift))
+		{
+			txt.text = "You can drive your car by using: W,A,S,D";
+			steering.Play ();
+		}
+	}
+
+	void StopText()
+	{
+		welcome.Stop ();
+		steering.Stop ();
+		gimble.Stop ();
+		shooting.Stop ();
+		jumping.Stop ();
+		strafing.Stop ();
+		nitro.Stop ();
+		flip.Stop ();
+		hull.Stop ();
+		txt.text = null;
 	}
 }
